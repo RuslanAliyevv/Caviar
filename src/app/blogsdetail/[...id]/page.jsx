ce8ef09a8671c4bc944ccbef6a252cf1e1c45b8e
Ruslan0770 <ruslania@code.edu.az>
2023-11-24 10:16:43 -0800

    blogdetailPhoto

--- src/app/blogsdetail/[...id]/page.jsx
+++ src/app/blogsdetail/[...id]/page.jsx
@@ -50,7 +50,12 @@ export default function BlogsDetail() {
             printer took a galley of type and scrambled it to make a type
             specimen book. It has survived not only five centuries, but also the
             leap into electronic typesetting,
-            <br /> Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply
+          </p>
+            <div className={styles.caviarPhoto}>
+              {/* for image */}
+            </div>
+         <p>
+         Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply
             dummy text of the printing and typesetting industry. Lorem Ipsum has
             been the industry's standard dummy text ever since the 1500s, when
             an unknown printer took a galley of type and scrambled it to make a
@@ -90,7 +95,7 @@ export default function BlogsDetail() {
             typesetting industry. Lorem Ipsum has been the industry's standard
             dummy text ever since the 1500s, when an unknown Lorem Ipsum is
             simply dummy text of the printing and typesetting industry.{" "}
-          </p>
+         </p>
         </div>
         <div className={styles.contentDown}>
           <div className={styles.down}>
